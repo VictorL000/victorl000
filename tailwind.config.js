b9662f788f58b1aaafdf99367ab0327cdbe82200
VictorL000 <victorrliu@gmail.com>
2023-09-10 18:02:22 -0400

    idk

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   mode: 'jit',
-  content: ["./*.html", "./pages/*.html"],
+  content: ["./*.html", "./public/pages/*.html"],
   theme: {
     extend: {},
     fontSize: {
